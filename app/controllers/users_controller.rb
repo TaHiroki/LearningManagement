8653bdb51e90cea0de3e8f580e6cfc999e97eacc
高雲大生 <takakumohiorki@takakumodaiseinoMacBook-Air.local>
2020-05-16 13:04:14 +0900

    fix @boxea

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,8 @@ class UsersController < ApplicationController
   def index
     @user = User.find(1)
 
-    @boxs = Array.new(10)
-    10.times{ |n| @boxs << '<div class="box count col-1 border border-dark" id='+"box#{n}"+'></div>' }
+    @boxes = Array.new(10)
+    10.times{ |n| @boxes << '<div class="box count col-1 border border-dark" id='+"box#{n}"+'></div>' }
                             
   end
 end
