d01fa648fc6ad36785768cbff5233208d493596f
高雲大生 <takakumohiorki@takakumodaiseinoMacBook-Air.local>
2020-05-18 13:41:23 +0900

    add comment new

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,4 +3,21 @@ class CommentsController < ApplicationController
     @comments = Comment.all
     @user = User.find(1)
   end
+
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.user_id = 1
+
+    redirect_to comments_path, notice: "コメントを投稿しました！"
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:comment)
+  end
 end
