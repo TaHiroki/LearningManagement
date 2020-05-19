aabcd64b320dfb37ed131aae382ebb25e2837649
高雲大生 <takakumohiorki@takakumodaiseinoMacBook-Air.local>
2020-05-19 16:13:37 +0900

    fix user/index .comment_box

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def index
     @user = User.find(1)
+    @comments = Comment.all.order(created_at: :desc).limit(3)
 
     @boxes = Array.new(10)
     10.times{ |n| @boxes << '<div class="box col-1 border border-dark" id='+"box#{n}"+'></div>' }
