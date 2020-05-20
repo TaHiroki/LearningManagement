671004ccff81af0f3f31946813e50aa20606a585
高雲大生 <takakumohiorki@takakumodaiseinoMacBook-Air.local>
2020-05-20 15:05:23 +0900

    add login,logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  root to: 'users#index'
-  resources :comments, :replies
+  root to: 'users#login'
+  resources :comments, only: [:index, :new, :show]
+  resources :replies, only: [:new]
   resources :users do
     collection do
       get :login
       post :loginpage
+      post :logout
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
