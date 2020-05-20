5a3f29884f763465fa27c4068f9d6d33c18e0738
高雲大生 <takakumohiorki@takakumodaiseinoMacBook-Air.local>
2020-05-21 01:45:44 +0900

    add delete_flag, fix user validates

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_20_084212) do
+ActiveRecord::Schema.define(version: 2020_05_20_153824) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,11 +54,11 @@ ActiveRecord::Schema.define(version: 2020_05_20_084212) do
     t.string "name"
     t.string "email"
     t.integer "first"
-    t.string "img"
     t.text "introduction"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.integer "delete_flag"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
