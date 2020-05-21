5a5a437966b2fb5dbd8273f41566e2b0a6756e88
高雲大生 <takakumohiorki@takakumodaiseinoMacBook-Air.local>
2020-05-22 01:18:49 +0900

    add friend

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,4 +1,13 @@
 class FriendsController < ApplicationController
   def index
+    users = Friend.where(master: @current_user).order(created_at: :desc)
+    @friends = []
+    users.each do |user|
+      @friends << User.find(user.user_id)
+    end
+  end
+
+  def show
+    @friend = User.find(params[:id])
   end
 end
