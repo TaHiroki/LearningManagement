bc45bdf1ea45e468bc62e4f196efebe6de2bae0e
高雲大生 <takakumohiorki@takakumodaiseinoMacBook-Air.local>
2020-05-22 17:48:54 +0900

    add friend btn

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -23,9 +23,15 @@ class FriendsController < ApplicationController
   end
 
   def create #フレンドになる
+    friend = Friend.new(master: @current_user.id, user_id: params[:id])
+    friend.save
+    redirect_to friends_path, notice: "ユーザー：#{User.find(params[:id]).name}さんと、フレンドになりました！"
   end
 
-  def delete #フレンドやめる
+  def destroy #フレンドやめる
+    friend = Friend.find_by(master: @current_user.id, user_id: params[:id])
+    friend.destroy
+    redirect_to friends_path, notice: "ユーザー：#{User.find(params[:id]).name}さんと、フレンドをやめました。"
   end
 
 end
