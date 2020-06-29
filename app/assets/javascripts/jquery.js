46af312e660f5eb2b41350533f551abc028f7326
高雲大生 <takakumohiorki@takakumodaiseinoMacBook-Air.local>
2020-06-29 21:08:27 +0900

    add sidebar move

--- app/assets/javascripts/jquery.js
+++ app/assets/javascripts/jquery.js
@@ -1,35 +1,56 @@
-$(function(){
+$(function () {
 
-  $('#explanationbtn').click(function(){
-    $('#modalbg').fadeIn();
-  });
+  //使い方ボタン
+  explanation();
 
-  $('#closebtn').click(function(){
-    $('#modalbg').fadeOut();
-  });
+  //サイドバーの開閉
+  sidebar();
 
-  $('#headbarbtn').click(function(){
-    $('.bar2').fadeIn(10);
-    $('#headbarbtn').fadeOut(10);
-  });
+  function explanation() {
+    $('#explanationbtn').click(function () {
+      $('#modalbg').fadeIn();
+    });
 
-  $('#headbarbtn2').click(function(){
-    $('.bar2').fadeOut(10);
-    $('#headbarbtn').fadeIn(10);
-  });
+    $('#closebtn').click(function () {
+      $('#modalbg').fadeOut();
+    });
+
+    $('#headbarbtn').click(function () {
+      $('.bar2').fadeIn(10);
+      $('#headbarbtn').fadeOut(10);
+    });
+
+    $('#headbarbtn2').click(function () {
+      $('.bar2').fadeOut(10);
+      $('#headbarbtn').fadeIn(10);
+    });
+  };
 
 
-
-  /* 　後の更新時の参考に、boxを押すと色が変わる
-
-  $('.box').click(function(){
-    if($(this).css('background-color') == 'rgb(255, 255, 255)' ){
-      $(this).css('background-color', 'rgb(135, 206, 250');
-    }else {
-      $(this).css('background-color', 'white');
-    }
-
-    var x = $(this).attr('id');
-  }); */
+  function sidebar() {
+    var duration = 300,
+      $side = $('.sidebar');
+    var $sideButton = $side.find('button')
+      .on('click', function () {
+        $side.toggleClass('open');
+        if ($side.hasClass('open')) {
+          $side.stop(true).animate({
+            left: '0px'
+          }, duration);
+          $side.find('.btn-r')
+            .css('display', 'none');
+          $side.find('.btn-l')
+            .css('display', 'block');
+        } else {
+          $side.stop(true).animate({
+            left: '-330px'
+          }, duration);
+          $side.find('.btn-r')
+            .css('display', 'block');
+          $side.find('.btn-l')
+            .css('display', 'none');
+        };
+      });
+  };
 
 });
